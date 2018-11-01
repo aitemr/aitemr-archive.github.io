2d5f4f2046d2aeeade82b5cfbf6613d431b82223
Islam Temirbek <aitemr16@gmail.com>
2018-11-01 20:29:25 +0600

    feat: update info

--- static/markdown.js
+++ static/markdown.js
@@ -7,28 +7,30 @@ Experienced mobile developer with 2+ years of experience. I am very proactive, g
 
 ## Experience
 
+### iOS Developer at Kolesa | Krisha | Market - job.kolesa.kz
+
+#### June 2018 (Almaty, Kazakhstan)
+
 ### Mobile Developer at ZeroToOneLabs - 021labs.com
 
 #### September 2016 - June 2018 (Almaty, Kazakhstan)
 
 - Built mobile application using React Native & Swift.
-- Developed back-end using Parse Server, Firebase & Node.js.
 - Developed mobile application **“Nommi”**, 4G Hotspot with Unlimited Wi-Fi Worldwide.
 - Developed **“Pifagor Intranet” mobile application**, for biggest educational center in central Asia.
 - Worked on development of **“Qamqor” mobile application**, that helps business owners to protect their businesses from illegal inspections.
-- Developed and designed **“Clementine” mobile application** from scratch, that provides food combining for weight loss & diet.
-- Developed and designed **“DFS” mobile application** from scratch, for programming interviews problems.
 
 ### iOS Mentor at nFactorial Incubator - n17r.com
 
 #### June 2017 - August 2017 (Astana, Kazakhstan)
 
 - Taught 70+ students to programming for iOS platform
+- Designed syllabus and prepared lectures & study materials on various topics
 
 ## Skills
 
 - <span>Objective-C</span> <span>Swift</span> <span>JavaScript</span> <span>TypeScript</span> 
-<span>React</span> <span>React Native</span> <span>Redux</span> <span>Mobx</span> <span>Firebase</span> <span>AWS</span>
+<span>React</span> <span>React Native</span> <span>Redux</span> <span>Firebase</span> <span>AWS</span>
 
 ## Education
 
@@ -36,18 +38,20 @@ Experienced mobile developer with 2+ years of experience. I am very proactive, g
 
 ### International Information Technology University, Almaty, Kazakhstan
 
-BSc Computer Systems and Software Engineering <small>(3.67 Overall GPA)</small>
+- BSc Computer Systems and Software Engineering <small>(3.1 Overall GPA)</small>
 
 ## Awards
 
-- 3<sup>rd</sup> place on **HalykFinTech Hackathon** (Feb 2016)
-- 4<sup>th</sup> place on **MegaAlmaty Hackathon** (Feb 2016)
+- 1<sup>st</sup> place on **MegaAlmaty Hackathon** (Feb 2016)
+- 2<sup>nd</sup> place on **Kolesa Internal Mobile Hackathon** (Sep 2018)
 
 ## Links
 
 [GitHub](https://github.com/aitemr)
+[Twitter](https://twitter.com/aitemr)
 [LinkedIn](https://www.linkedin.com/in/aitemr/)
 [Telegram](http://telegram.me/aitemr)
+[Medium](https://medium.com/@aitemr)
 `;
 
 // eslint-disable-next-line import/prefer-default-export
